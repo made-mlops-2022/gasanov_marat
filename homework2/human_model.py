18c5b15388647f56f256fc93c40a83a940de95a9
Marat Gasanov <maratgasanov6@gmail.com>
2022-11-24 13:17:43 +0300

    fix validation

--- homework2/human_model.py
+++ homework2/human_model.py
@@ -24,53 +24,53 @@ class HumanModel(BaseModel):
     ca: int
     thal: int
 
-    # @validator('age')
-    # def age_must_positive(cls, age):
-    #     if age < 0:
-    #         raise ValueError('age must be positive')
-    #
-    # @validator('sex')
-    # def sex_categorical(cls, sex):
-    #     if sex != 0 and sex != 1:
-    #         raise ValueError('sex must be 0 or 1')
-    #
-    # @validator('cp')
-    # def cp_categorical(cls, cp):
-    #     if cp not in [0, 1, 2, 3]:
-    #         raise ValueError(f'cp must be in {[0, 1, 2, 3]}')
-    #
-    # @validator('fbs')
-    # def fbs_categorical(cls, fbs):
-    #     values = [0, 1]
-    #     if fbs not in values:
-    #         raise ValueError(f'fbs must be int {values}')
-    #
-    # @validator('restecg')
-    # def restecg_categorical(cls, restecg):
-    #     values = [0, 1, 2]
-    #     if restecg not in values:
-    #         raise ValueError(f'restecg must be int {values}')
-    #
-    # @validator('exang')
-    # def exang_categorical(cls, exang):
-    #     values = [0, 1]
-    #     if exang not in values:
-    #         raise ValueError(f'exang must be int {values}')
-    #
-    # @validator('slope')
-    # def slope_categorical(cls, slope):
-    #     values = [0, 1, 2]
-    #     if slope not in values:
-    #         raise ValueError(f'slope must be int {values}')
-    #
-    # @validator('ca')
-    # def ca_categorical(cls, ca):
-    #     values = [0, 1, 2, 3]
-    #     if ca not in values:
-    #         raise ValueError(f'ca must be int {values}')
-    #
-    # @validator('thal')
-    # def thal_categorical(cls, thal):
-    #     values = [0, 1, 2]
-    #     if thal not in values:
-    #         raise ValueError(f'thal must be int {values}')
+    @validator('age')
+    def age_must_positive(cls, age):
+        if age < 0:
+            raise ValueError('age must be positive')
+
+    @validator('sex')
+    def sex_categorical(cls, sex):
+        if sex != 0 and sex != 1:
+            raise ValueError('sex must be 0 or 1')
+
+    @validator('cp')
+    def cp_categorical(cls, cp):
+        if cp not in [0, 1, 2, 3]:
+            raise ValueError(f'cp must be in {[0, 1, 2, 3]}')
+
+    @validator('fbs')
+    def fbs_categorical(cls, fbs):
+        values = [0, 1]
+        if fbs not in values:
+            raise ValueError(f'fbs must be int {values}')
+
+    @validator('restecg')
+    def restecg_categorical(cls, restecg):
+        values = [0, 1, 2]
+        if restecg not in values:
+            raise ValueError(f'restecg must be int {values}')
+
+    @validator('exang')
+    def exang_categorical(cls, exang):
+        values = [0, 1]
+        if exang not in values:
+            raise ValueError(f'exang must be int {values}')
+
+    @validator('slope')
+    def slope_categorical(cls, slope):
+        values = [0, 1, 2]
+        if slope not in values:
+            raise ValueError(f'slope must be int {values}')
+
+    @validator('ca')
+    def ca_categorical(cls, ca):
+        values = [0, 1, 2, 3]
+        if ca not in values:
+            raise ValueError(f'ca must be int {values}')
+
+    @validator('thal')
+    def thal_categorical(cls, thal):
+        values = [0, 1, 2]
+        if thal not in values:
+            raise ValueError(f'thal must be int {values}')
